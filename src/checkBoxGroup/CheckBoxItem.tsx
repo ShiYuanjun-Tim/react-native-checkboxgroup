7716a50364f34caa3dc36148d39f1546c961b8a2
Timshi <syj_news@163.com>
2017-09-23 01:38:15 +0800

    dsa

--- src/checkBoxGroup/CheckBoxItem.tsx
+++ src/checkBoxGroup/CheckBoxItem.tsx
@@ -3,33 +3,32 @@
  */
 import * as React from "react";
 import {Text, View} from "react-native";
-import BitSwitcher from "./BitSwitcher"
+import BitSwitcher from "./BitSwitcher";
 import ReactElement = React.ReactElement;
 
-const IDENTIFIER =Symbol("Selectable")
+const IDENTIFIER = Symbol("Selectable")
 
 export interface  Selectable {
+	/*select 和 deselect 都是内部组件间调用的 toggle 才是唯一暴露给外部使用的*/
 	select(): void;
 	deselect(): void;
 	toggle(): void;
 }
 
-function getSelected(isSelected: boolean) {
 
+interface SelectableProps {
+	selectedChanged: (key: string, isSelected: boolean) => void;
+	ref: string|Function;
+	identifier: string;
+	children: React.ReactNode;
 }
 
-interface CheckBoxItemProps {
-	selectedChanged?: (key:string,isSelected: boolean) => void;
-	ref?: string|Function;
-	identifier:string;
-}
+export class CheckBoxItem extends React.Component<SelectableProps,{isSelected: boolean}> implements Selectable {
+	static IDENTIFIER = IDENTIFIER
 
-export class CheckBoxItem extends React.Component<CheckBoxItemProps,{isSelected: boolean}> implements Selectable {
-	static IDENTIFIER=IDENTIFIER
-
-	static defaultProps= {
-			selectedChanged(k:string,isSelected: boolean): void{
-			}
+	static defaultProps = {
+		selectedChanged(k: string, isSelected: boolean): void{
+		}
 	}
 
 	state = {
@@ -45,22 +44,19 @@ export class CheckBoxItem extends React.Component<CheckBoxItemProps,{isSelected:
 	}
 
 	toggle() {
-		let current=this.state.isSelected
+		let current = this.state.isSelected
 		if (current) {
 			this.deselect()
 		} else {
 			this.select()
 		}
-		this.props.selectedChanged&&this.props.selectedChanged(this.props.identifier,!current)
+		this.props.selectedChanged && this.props.selectedChanged(this.props.identifier, !current)
 	}
 
-	private isChild(){
-
-	}
 
 	render() {
 		console.log("CheckboxItem render")
-		return <View style={{flexDirection:"row"}} >
+		return <View style={{flexDirection:"row"}}>
 			<Text onPress={()=>{this.toggle()}}>{this.state.isSelected ? "On" : "Off"} {this.props.identifier} </Text>
 			{this.props.children}
 		</View>
@@ -69,39 +65,39 @@ export class CheckBoxItem extends React.Component<CheckBoxItemProps,{isSelected:
 }
 
 interface  Prop {
-	selectedChanged?: (key:string,isSelected: boolean) => void;
+	selectedChanged?: (key: string, isSelected: boolean) => void;
 	children: React.ReactNode;
-	identifier?:string;
+	identifier?: string;
 }
 
 export class CheckBoxGroup extends React.Component<Prop,{isSelected: boolean}> implements Selectable {
-	static IDENTIFIER=IDENTIFIER
+	static IDENTIFIER = IDENTIFIER
 
 	state = {
 		isSelected: false
 	};
 
-	private bs:BitSwitcher<string>;
-	private keys:string[];
+	private bs: BitSwitcher<string>;
+	private keys: string[];
 	private _items: Map<string,Selectable> = new Map();
 
-	private addItem(key:string,item: Selectable): void {
+	private addItem(key: string, item: Selectable): void {
 		if (!this._items.has(key)) {
-			this._items.set(key,item);
+			this._items.set(key, item);
 		}
 	}
 
 	constructor(props: Prop, context: any) {
 		super(props, context);
 
-		this.keys=React.Children.map(this.props.children, (e) =>
-			(this.isSelectableComp(e)?"Group_":"Item_")+Math.ceil( Math.random()*10000000))
+		this.keys = React.Children.map(this.props.children, (e) =>
+		(this.isSelectableComp(e) ? "Group_" : "Item_") + Math.ceil(Math.random() * 10000000))
 		this.bs = new BitSwitcher(...this.keys)
 	}
 
 	select(): void {
 		this.setState({isSelected: true})
-		this._items.forEach((v,k)=> {
+		this._items.forEach((v, k) => {
 			this.bs.on(k)
 			v.select();
 		})
@@ -109,7 +105,7 @@ export class CheckBoxGroup extends React.Component<Prop,{isSelected: boolean}> i
 
 	deselect(): void {
 		this.setState({isSelected: false})
-		this._items.forEach((v,k) => {
+		this._items.forEach((v, k) => {
 			this.bs.off(k)
 			v.deselect();
 		})
@@ -123,60 +119,61 @@ export class CheckBoxGroup extends React.Component<Prop,{isSelected: boolean}> i
 		} else {
 			this.deselect()
 		}
-		this.props.selectedChanged&&this.props.selectedChanged(this.props.identifier||"",isSelectedNext)
+		this.props.selectedChanged && this.props.selectedChanged(this.props.identifier || "", isSelectedNext)
 	}
 
-	private isSelectableComp(reactChild:React.ReactChild){
-		if (reactChild&&(reactChild as React.ReactElement<Prop>).type){
-			let convert=reactChild as React.ReactElement<Prop>;
-			let isChildSelectable = (typeof convert.type=="function")
-				&&convert.type.name =="CheckBoxGroup"
-				&&(convert.type as any).IDENTIFIER==IDENTIFIER
+	private isSelectableComp(reactChild: React.ReactChild) {
+		if (reactChild && (reactChild as React.ReactElement<Prop>).type) {
+			let convert = reactChild as React.ReactElement<Prop>;
+			let isChildSelectable = (typeof convert.type == "function")
+				&& convert.type.name == "CheckBoxGroup"
+				&& (convert.type as any).IDENTIFIER == IDENTIFIER
 			return isChildSelectable
 		}
 		return false
 	}
 
+	private selectedChanged = (childKey: string, isSelected: boolean) => {
+		isSelected ? this.bs.on(childKey) : this.bs.off(childKey);
+		let nextGrpState = this.bs.isAllOn();
+		if (nextGrpState != this.state.isSelected) {
+			this.setState({isSelected: nextGrpState})
+			this.props.selectedChanged && this.props.selectedChanged(this.props.identifier || "", nextGrpState)
+		}
+	}
+
+	private enrichChildProps = (ownKey: string) => {
+		console.log("ownKey", ownKey)
+		return {
+			identifier     : ownKey,
+			key            : ownKey,
+			ref            : (item: Selectable) => {
+				item && this.addItem(ownKey, item)
+			},
+			selectedChanged: this.selectedChanged
+		}
+	};
+
 	render() {
 		console.log("CheckboxGroup render")
 
-		let that = this;
-		let enrichProps= (ownKey:string)=>{
-			console.log("ownKey",ownKey)
-			return {
-				identifier:ownKey,
-				key:ownKey,
-				ref:(item:Selectable)=>{
-					item&&(that as any).addItem( ownKey,item)
-				},
-				selectedChanged:(childKey:string,isSelected:boolean)=>{
-					isSelected?(that as any).bs.on(childKey):(that as any).bs.off(childKey);
-					let nextGrpState = (that as any).bs.isAllOn();
-					if(nextGrpState!=(that as any).state.isSelected){
-						(that as any).setState({isSelected:nextGrpState})
-						(that as any).props.selectedChanged&&(that as any).props.selectedChanged(ownKey,nextGrpState)
-					}
-				}
-			}
-		};
-
-		let children=	React.Children.map(this.props.children, (reactChild, index) => {
+		let children = React.Children.map(this.props.children, (reactChild, index) => {
 			let key = this.keys[index];
-			if(this.isSelectableComp(reactChild)){
-				return React.cloneElement((reactChild as React.ReactElement<any>),{...enrichProps(key)})
-			}else{
+			if (this.isSelectableComp(reactChild)) {
+				return React.cloneElement((reactChild as React.ReactElement<any>), {...this.enrichChildProps(key)})
+			} else {
 				return (
-					<CheckBoxItem {...enrichProps(key)}>
+					<CheckBoxItem {...this.enrichChildProps(key)}>
 						{reactChild}
 					</CheckBoxItem>
 				)
 			}
 
-			})
+		})
 
-		return <View style={{ }} >
+		return <View style={{ }}>
 			<Text onPress={()=>{this.toggle()}}>{this.state.isSelected ? "On" : "Off"} {this.props.identifier} </Text>
-			 { children }
+			{ children }
 		</View>
 	}
 
